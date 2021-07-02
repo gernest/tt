a401b260cda2370464750701753d93c3975cfbba
gernest <geofreyernest@live.com>
2021-07-02 22:10:31 +0300

    add load balancer and reverse proxy package

---
+++ pkg/balance/load_balance.go
@@ -0,0 +1,92 @@
+package balance
+
+import (
+	"errors"
+	"net/url"
+
+	"github.com/gernest/tt/api"
+	"github.com/smallnest/weighted"
+)
+
+var ErrConfigNotFound = errors.New("balance: Missing load balancer configuration")
+
+type Target struct {
+	URL *url.URL
+}
+
+type Balance interface {
+	Next() *Target
+}
+
+type balanceFn func() *Target
+
+func (fn balanceFn) Next() *Target {
+	return fn()
+}
+
+func target(w *api.WeightedAddr) (*Target, error) {
+	u, err := url.Parse(w.Addr.Address)
+	if err != nil {
+		return nil, err
+	}
+	return &Target{URL: u}, nil
+}
+
+func FromRoute(r *api.Route) (Balance, error) {
+	if r.Endpoint != nil {
+		return FromWeightedAddr(api.Route_RandomWeighted, r.Endpoint)
+	}
+	return FromWeightedAddr(r.LoadBalanceAlgo, r.LoadBalance...)
+}
+
+func FromWeightedAddr(algo api.Route_LoadBalanceAlgo, targets ...*api.WeightedAddr) (Balance, error) {
+	if len(targets) == 1 {
+		t, err := target(targets[0])
+		if err != nil {
+			return nil, err
+		}
+		return balanceFn(func() *Target {
+			return t
+		}), nil
+	}
+	switch algo {
+	case api.Route_RoundRobinWeighted:
+		w := &weighted.RRW{}
+		for _, v := range targets {
+			t, err := target(v)
+			if err != nil {
+				return nil, err
+			}
+			w.Add(t, int(v.Weight))
+		}
+		return balanceFn(func() *Target {
+			return w.Next().(*Target)
+		}), nil
+	case api.Route_SmoothWeighted:
+		w := &weighted.SW{}
+		for _, v := range targets {
+			t, err := target(v)
+			if err != nil {
+				return nil, err
+			}
+			w.Add(t, int(v.Weight))
+		}
+		return balanceFn(func() *Target {
+			return w.Next().(*Target)
+		}), nil
+
+	case api.Route_RandomWeighted:
+		w := &weighted.RandW{}
+		for _, v := range targets {
+			t, err := target(v)
+			if err != nil {
+				return nil, err
+			}
+			w.Add(t, int(v.Weight))
+		}
+		return balanceFn(func() *Target {
+			return w.Next().(*Target)
+		}), nil
+	}
+	return nil, ErrConfigNotFound
+}
---
+++ pkg/reverse/http_reverse_proxy.go
@@ -0,0 +1,82 @@
+package reverse
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/gernest/tt/pkg/balance"
+)
+
+type Director interface {
+	// Director is a function which modifies
+	// the request into a new request to be sent
+	// using Transport. Its response is then copied
+	// back to the original client unmodified.
+	// Director must not access the provided Request
+	// after returning.
+	Request(r *http.Request)
+}
+
+type DirectorFunc func(r *http.Request)
+
+func (df DirectorFunc) Request(r *http.Request) {
+	df(r)
+}
+
+// Request modifies req to point to target
+func Request(target *url.URL, req *http.Request) {
+	targetQuery := target.RawQuery
+	req.URL.Scheme = target.Scheme
+	req.URL.Host = target.Host
+	req.URL.Path, req.URL.RawPath = joinURLPath(target, req.URL)
+	if targetQuery == "" || req.URL.RawQuery == "" {
+		req.URL.RawQuery = targetQuery + req.URL.RawQuery
+	} else {
+		req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+	}
+	if _, ok := req.Header["User-Agent"]; !ok {
+		// explicitly disable User-Agent so it's not set to default value
+		req.Header.Set("User-Agent", "")
+	}
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
+func joinURLPath(a, b *url.URL) (path, rawpath string) {
+	if a.RawPath == "" && b.RawPath == "" {
+		return singleJoiningSlash(a.Path, b.Path), ""
+	}
+	// Same as singleJoiningSlash, but uses EscapedPath to determine
+	// whether a slash should be added
+	apath := a.EscapedPath()
+	bpath := b.EscapedPath()
+
+	aslash := strings.HasSuffix(apath, "/")
+	bslash := strings.HasPrefix(bpath, "/")
+
+	switch {
+	case aslash && bslash:
+		return a.Path + b.Path[1:], apath + bpath[1:]
+	case !aslash && !bslash:
+		return a.Path + "/" + b.Path, apath + "/" + bpath
+	}
+	return a.Path + b.Path, apath + bpath
+}
+
+func DirectorFromLoadBalance(ba balance.Balance) Director {
+	return DirectorFunc(func(r *http.Request) {
+		target := ba.Next()
+		Request(target.URL, r)
+	})
+}
