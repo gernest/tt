e6676df768d016e7f2625d1e11965f2a5c1f6d90
gernest <geofreyernest@live.com>
2021-07-05 14:21:03 +0300

    add common.HeaderMap impl

--- pkg/xhttp/xwasm/imports_handler.go
+++ pkg/xhttp/xwasm/imports_handler.go
@@ -1,3 +1,16 @@
 package xwasm
 
-type Imports struct{}
+import (
+	"net/http"
+
+	"mosn.io/proxy-wasm-go-host/proxywasm/common"
+)
+
+type Imports struct {
+	Request  *http.Request
+	Response http.ResponseWriter
+}
+
+func (i *Imports) GetHttpRequestHeader() common.HeaderMap {
+	return &Header{head: i.Request.Header}
+}
