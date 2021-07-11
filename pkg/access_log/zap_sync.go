902281532c4cebe11f5285ceaa14eefc05e85583
gernest <geofreyernest@live.com>
2021-07-11 15:11:38 +0300

    add zap Sync

--- pkg/access_log/zap_sync.go
+++ pkg/access_log/zap_sync.go
@@ -2,6 +2,25 @@ package accesslog
 
 import "go.uber.org/zap"
 
-func ZapEntry(lg *zap.Logger, e *Entry) {
+var _ Sync = (*Zap)(nil)
 
+type Zap struct {
+	Logger *zap.Logger
+}
+
+func (Zap) sync() {}
+
+func (z *Zap) Sync(e *Entry) {
+	defer e.Release()
+	z.Logger.Debug(e.Request.Path, e.fields()...)
+}
+
+func (e *Entry) fields() (ls []zap.Field) {
+	ls = append(ls,
+		zap.Int32("status", e.Response.StatusCode),
+		zap.String("route", e.Route),
+		zap.String("service", e.Service),
+		zap.String("host", e.VirtualHost),
+	)
+	return
 }
